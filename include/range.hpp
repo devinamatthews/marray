e50afb4e75ba5395612c08186d80ccee49d3f833
Devin Matthews <damatthews@smu.edu>
2020-04-01 11:22:26 -0500

    A number of fixes and performance improvements.

--- include/range.hpp
+++ include/range.hpp
@@ -240,22 +240,48 @@ auto range(T to)
 template <typename T, typename U>
 auto rangeN(T from, U N)
 {
-    typedef typename detail::underlying_type_if<typename std::common_type<T,U>::type>::type V;
+    typedef decltype(std::declval<T>() + std::declval<U>()) V0;
+    typedef typename detail::underlying_type_if<V0>::type V;
     return range_t<V>{V(from), V(from+N)};
 }
 
 template <typename T, typename U>
 auto range(T from, U to)
 {
-    typedef typename detail::underlying_type_if<typename std::common_type<T,U>::type>::type V;
+    typedef decltype(std::declval<T>() + std::declval<U>()) V0;
+    typedef typename detail::underlying_type_if<V0>::type V;
     return range_t<V>{(V)from, (V)to};
 }
 
 template <typename T, typename U, typename V>
 auto range(T from, U to, V delta)
 {
-    typedef typename detail::underlying_type_if<typename std::common_type<T,U,V>::type>::type W;
+    typedef decltype(std::declval<T>() + std::declval<U>() + std::declval<V>()) W0;
+    typedef typename detail::underlying_type_if<W0>::type W;
     return range_t<W>{(W)from, (W)to, (W)delta};
+}
+
+template <typename T>
+auto reversed_range(T to)
+{
+    typedef typename detail::underlying_type_if<T>::type U;
+    return range_t<U>{U(to-1), U(-1), U(-1)};
+}
+
+template <typename T, typename U>
+auto reversed_rangeN(T from, U N)
+{
+    typedef decltype(std::declval<T>() + std::declval<U>()) V0;
+    typedef typename detail::underlying_type_if<V0>::type V;
+    return range_t<V>{V(from-1), V(from-N-1), V(-1)};
+}
+
+template <typename T, typename U>
+auto reversed_range(T from, U to)
+{
+    typedef decltype(std::declval<T>() + std::declval<U>()) V0;
+    typedef typename detail::underlying_type_if<V0>::type V;
+    return range_t<V>{V(to-1), V(from-1), V(-1)};
 }
 
 }
